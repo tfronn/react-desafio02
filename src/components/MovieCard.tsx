3b9b7d058a3c6249daf5b37133cf38286bb9e3dc
Kelvin Camilo <keeelvincamilo@hotmail.com>
2022-04-17 02:05:17 -0300

    desafio finalizado

--- src/components/MovieCard.tsx
+++ src/components/MovieCard.tsx
@@ -1,21 +1,18 @@
-import { Star, Clock } from 'react-feather';
+import { Star, Clock } from 'react-feather'
 
-import '../styles/movie-card.scss';
+import '../styles/movie-card.scss'
 
 interface MovieCardProps {
-  title: string;
-  poster: string;
-  rating: string;
-  runtime: string;
+  title: string
+  poster: string
+  rating: string
+  runtime: string
 }
 
 export function MovieCard(props: MovieCardProps) {
   return (
     <div className="movie-card">
-      <img
-        src={props.poster}
-        alt={props.title}
-      />
+      <img src={props.poster} alt={props.title} />
 
       <div>
         <div className="movie-info">
